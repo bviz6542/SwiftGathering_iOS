93370171614a9f648dd5ac33ba47f13b048dbd0c
정준우 <80523328+bviz6542@users.noreply.github.com>
2024-04-07 18:16:09 +0900

    Rabbitmqhandler sending and receiving datas (#6)
    * feature: rabbitmqhandler
    
    * fix: test passed

--- SwiftGathering_iOSTests/SwiftGathering_iOSTests.swift
+++ SwiftGathering_iOSTests/SwiftGathering_iOSTests.swift
@@ -12,6 +12,7 @@ final class SwiftGathering_iOSTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        continueAfterFailure = false
     }
 
     override func tearDownWithError() throws {
