3ad2dc6a38d7637c10b7bf496f55c44e95d758fc
bviz6542 <bviz6542@gmail.com>
2024-05-19 18:03:29 +0900

    feature: rx on friends tableview

--- SwiftGathering_iOS/Presentation/AuthenticationScene/Login/LoginViewController.swift
+++ SwiftGathering_iOS/Presentation/AuthenticationScene/Login/LoginViewController.swift
@@ -29,6 +29,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
         registerKeyboardNotifications()
     }
     
