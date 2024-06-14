6ce0fe05a249841f3178d585c5a729018cf1f904
정준우 <80523328+bviz6542@users.noreply.github.com>
2024-06-14 19:35:31 +0900

    feature: HTTPConstants

---
+++ SwiftGathering_iOS/Infra/Networking/HTTP/HTTPConstant.swift
@@ -0,0 +1,12 @@
+//
+//  HTTPConstant.swift
+//  SwiftGathering_iOS
+//
+//  Created by mraz on 6/14/24.
+//
+
+struct HTTPConstant {
+    static let defaultScheme = "http"
+    static let defaultHost = "localhost"
+    static let defaultPort = 8080
+}
